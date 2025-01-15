c4674fd19570b0c2d5d599746d6a289dd7e2a29f
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2025-01-15 12:28:01 -0500

    html and css styling  for site-header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddFormComponent } from './add-form/add-form.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'home',pathMatch:"full"},
+  {path:"home",component:HomeComponent},
+  {path:"event-list",component:EventListComponent},
+  {path:"add-form",component:AddFormComponent},
+  { path: 'add-form/:id', component: AddFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
